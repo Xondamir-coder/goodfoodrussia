f6284a0836ead431a13f6af946a5b652d3d5114e
Xondamir <xondamirnazrullayev@gmail.com>
2026-05-09 14:47:22 +0500

    scroll fix

--- app/plugins/lenis.client.js
+++ app/plugins/lenis.client.js
@@ -1,6 +1,8 @@
 import Lenis from 'lenis';
 
 export default defineNuxtPlugin(() => {
+  const router = useRouter();
+
   const lenis = new Lenis({
     autoRaf: true,
     smoothWheel: true
@@ -9,6 +11,14 @@ export default defineNuxtPlugin(() => {
   const toggleLenis = () => {
     lenis.isStopped ? lenis.start() : lenis.stop();
   };
+
+  router.beforeEach(() => {
+    if (lenis.actualScroll) {
+      lenis.scrollTo(0, {
+        immediate: true
+      });
+    }
+  });
 
   return {
     provide: {
